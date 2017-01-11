e660c42e78ee308a39dfb8d54a8b0a0338860201
M. Alex Boyd <mboyd8@students.towson.edu>
2017-01-10 20:36:31 -0500

    Searchbar and funtimes
    - Added link to material design icons
    - Updated Characters page to include search bar. Search bar filters
    results based on character name only right now.
    - Added resetPage()
    
    - Modified some styling on base css across all sites.

--- src/pages/characters/characters.ts
+++ src/pages/characters/characters.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CharacterPage } from '../character/character';
-import { NavController } from 'ionic-angular';
+import { CharacterFilterPipe } from '../../app/pipes/character-filter.pipe';
 
 @Component({
   selector: 'page-characters',
@@ -9,8 +9,10 @@ import { NavController } from 'ionic-angular';
 
 export class Characters {
   selectedItem: any;
+  searchTerm : string = ' ';
   characters: CharacterPage[];
   items: Array<{character: CharacterPage}>;
+  resetItems: Array<{character: CharacterPage}>; // used to reset items after filtering in search.
   constructor() {
 
     // Let's populate this page with character list
@@ -51,12 +53,31 @@ export class Characters {
       character : this.characters[i],
       });
     }
+
+    this.resetItems = this.items;
+  }
+
+  getSearch(ev : any)
+  {
+    this.items = this.resetItems; // reset allowing for all to  be searched
+    let searchTerm = ev.target.value;
+    if(searchTerm && searchTerm.trim() != '') // not empty
+    {
+      console.log("Search term" + searchTerm);
+      this.items = this.items.filter( (item) => {
+        return (item.character.getName().toLowerCase().indexOf(searchTerm.toLowerCase()) > -1);
+      })
+    }
+  }
+
+
+  resetPage(ev : any) // don't think this is needed, but not 100% sure.
+  {
+    this.items = this.resetItems;
   }
 
 
 
-  itemTapped($event, item) {
-    console.log(item.getName());
-  }
+
 
 }
